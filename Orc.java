b89ca95c4b7c666f2bc4524f27b6760b3e580a0d
eduardonk9999 <thiago.novak7@gmail.com>
2025-08-06 18:11:52 -0300

    orc

---
+++ Orc.java
@@ -0,0 +1,9 @@
+import java.util.List;
+
+public class Orc  extends Personagem{
+    public Orc(String nome, String raca, List<String> habilidades, String arma, String defesa) {
+        super(nome, raca, habilidades, arma, defesa);
+    }
+
+
+}
