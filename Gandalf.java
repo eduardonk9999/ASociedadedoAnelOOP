681454cff1244fd67e9c96de9b2de5036bfb168c
Eduardo Silva <thiago.novak7@gmail.com>
2025-08-11 14:06:31 -0300

    personagem gandalf

--- Gandalf.java
+++ Gandalf.java
@@ -1,6 +1,11 @@
 
 public class Gandalf extends Personagem {
     public Gandalf(String nome) {
-        
+        super("Gandalf", "Mago", List.of("Cajado", "espada Glamdrin"), "Magia");
+    }
+
+    @Override
+    public void ataqueEspecial(Personagem alvo) {
+         System.out.println(getNome() + " ataca ferozmente " + alvo.getNome() + " com um golpe poderoso!")
     }
 }
